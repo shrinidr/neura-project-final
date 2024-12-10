34f4e11f01f6a5b8ebfa097504febb5c8ec185cd
James Liu <jamesliu@bu.edu>
2024-12-10 09:40:16 -0500

    Added footer + extra text sections to bottom

--- rushil-neura-project/src/components/Bottom.tsx
+++ rushil-neura-project/src/components/Bottom.tsx
@@ -30,7 +30,74 @@ const Bottom = ({ text1, text2, text3, text4 }: Props) => {
                         </button>
                     </Link>
                 </div>
+                <div className="LinkBox">
+                    <div className="row">
+                        <div className="section">
+                            <h2 className="BoxOne">Who Are We?</h2>
+                            <div className="package">
+                            <p style={{ lineHeight: '19px' }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br />
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br />
+                            </p>
+                                <Link to="/home">
+                                    <button className="btn" type="button">
+                                        <strong> Filler </strong>
+                                        <div id="container-stars">
+                                            <div id="stars"></div>
+                                        </div>
+                                        <div id="glow">
+                                            <div className="circle"></div>
+                                            <div className="circle"></div>
+                                        </div>
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="section">
+                            <h2 className="BoxTwo">Our Mission</h2>
+                            <div className="package">
+                            <p style={{ lineHeight: '19px' }}>
+                                Two roads diverged in a yellow wood <br />
+                                And sorry I could not travel both <br />
+                                And be one traveler, long I stood <br />
+                                And looked down one as far as I could <br />
+                            </p>
+                                <Link to="/home">
+                                    <button className="btn" type="button">
+                                        <strong> Filler </strong>
+                                        <div id="container-stars">
+                                            <div id="stars"></div>
+                                        </div>
+                                        <div id="glow">
+                                            <div className="circle"></div>
+                                            <div className="circle"></div>
+                                        </div>
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+            <footer className="footer">
+                <div className="footer-content">
+                    <p>&copy; 2024 Your Company Name </p> 
+                    <p>All rights reserved.</p>
+                    <div className="footer-links">
+                        <Link to="/privacy">Privacy Policy</Link>
+                        <Link to="/terms">Terms of Service</Link>
+                        <Link to="/contact">Contact</Link> 
+                        <Link to="/blog">Blog</Link>
+                        <Link to="/phonenumber">Call Us</Link>
+                        <Link to="/email">Email Us</Link>
+                    </div>
+                </div>
+            </footer>
+
         </div>
     );
 }
