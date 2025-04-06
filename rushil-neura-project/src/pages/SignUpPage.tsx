1a0003df798b6b77eeccbb8f0dc333e23fcac352
shrinidr <rushilk@bu.edu>
2025-04-05 21:29:46 -0400

    one more change

--- rushil-neura-project/src/pages/SignUpPage.tsx
+++ rushil-neura-project/src/pages/SignUpPage.tsx
@@ -1,7 +1,7 @@
 import { RedirectToSignUp } from "@clerk/clerk-react";
 
 const SignUpPage = () => {
-  return <RedirectToSignUp redirectUrl="/callBackPage" />;
+  return <RedirectToSignUp redirectUrl="/call-back" />;
 };
 
 export default SignUpPage;
