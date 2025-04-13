8b4189906e01490d47e64cd4d37f566ec91e4f1c
shrinidr <rushilk@bu.edu>
2025-04-13 15:08:38 -0400

    bit more changes

--- rushil-neura-project/src/components/Footer.tsx
+++ rushil-neura-project/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (<div>
                 <footer className="footer">
                 <div className="footer-content">
-                <div>
+                <div style = {{marginLeft: '-20px'}}>
                     <img src="/neura-removebg-preview.png" style={{marginRight: '6px'}} />
                     <Link to ='/'>  <p id="title" style = {{marginLeft: '0px'}}><b> Neura </b> </p> </Link> 
                 </div>
